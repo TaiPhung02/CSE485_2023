a7df454f7adf718705563efe43c7dd2633d4544a
people0507 <2051063581@e.tlu.edu.vn>
2023-02-25 14:15:57 +0700

    update button
    add category button

--- SourceCode/admin/process_add_category.php
+++ SourceCode/admin/process_add_category.php
@@ -1,1 +1,11 @@
-<!-- sada -->
+<?php
+    include '../connectdb.php';
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $tentloai = $_POST['txttentloai'];
+        
+        $sql = "INSERT INTO `theloai`(`ten_tloai`) VALUES ('$tentloai')";
+        $result = mysqli_query($conn,$sql);
+        header('Location: Category.php');
+        
+    }
+?>
