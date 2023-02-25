a094306097713ed3f733a0320fd9dbf0d85c55e7
people0507 <2051063581@e.tlu.edu.vn>
2023-02-25 16:03:30 +0700

    update add function
    add category button

--- SourceCode/admin/edit_author.php
+++ SourceCode/admin/edit_author.php
@@ -32,10 +32,10 @@ include '../connectdb.php';
                         <a class="nav-link" href="../index.php">Trang ngoài</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
+                        <a class="nav-link " href="category.php">Thể loại</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="author.php">Tác giả</a>
+                        <a class="nav-link active fw-bold" href="author.php">Tác giả</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="article.php">Bài viết</a>
