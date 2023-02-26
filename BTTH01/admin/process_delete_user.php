b9a4933439f51f437013ff3f803f79b976f7c641
HungHoang1 <2051063705@e.tlu.edu.vn>
2023-02-26 14:39:14 +0700

    update sửa xóa người dùng

--- BTTH01/admin/process_delete_user.php
+++ BTTH01/admin/process_delete_user.php
@@ -1,7 +1,7 @@
 <?php
     include '../connectdb.php';
-        $mabviet = $_GET['id_user'];
-        $sql = "DELETE FROM `tb_user` WHERE `id` = '$id';";
+        $idnguoidung = $_GET['id_user'];
+        $sql = "DELETE FROM `users` WHERE `id_nguoidung` = '$idnguoidung';";
         $result = mysqli_query($conn,$sql);
         header('Location:user.php');
 ?>
